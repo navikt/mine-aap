a485d530ef963d58d41bca0b406fd3c881a82917
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2022-08-31 12:46:15 +0200

    :speech_balloon: Oppdaterer tekster for 'Hva skjer nå'

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,15 @@
 import { Attachment, Information } from '@navikt/ds-icons';
-import { Alert, BodyShort, Button, Heading, Link, LinkPanel, Panel } from '@navikt/ds-react';
+import {
+  Alert,
+  BodyLong,
+  BodyShort,
+  Button,
+  Heading,
+  Label,
+  Link,
+  LinkPanel,
+  Panel,
+} from '@navikt/ds-react';
 import type { GetServerSidePropsResult, NextPageContext } from 'next';
 import { useRouter } from 'next/router';
 import { useMemo } from 'react';
@@ -107,13 +117,62 @@ const Index = ({ søknader, dokumenter, mellomlagredeSøknader }: PageProps) => 
       )}
 
       <Section>
-        <PanelWithTopIcon title={formatMessage('dineOppgaver.tittel')} icon={<Information />}>
-          <BodyShort spacing>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</BodyShort>
-        </PanelWithTopIcon>
-        <PanelWithTopIcon title={formatMessage('hvaGjorVi.tittel')} icon={<Information />}>
-          <BodyShort spacing>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</BodyShort>
-        </PanelWithTopIcon>
-        <PanelWithTopIcon title={formatMessage('dokumentoversikt.tittel')} icon={<Attachment />}>
+        <Panel border>
+          <Heading level="2" size="medium" spacing>
+            Vi har mottatt søknaden din, hva skjer nå?
+          </Heading>
+          <ul>
+            <li>
+              <Label>Vi sjekker om vi har nok helseopplysninger</Label>
+              <BodyLong spacing>
+                Hvis vi har behov for flere helseopplysninger, vil vi bruke informasjonen du ga oss
+                i søknaden til å bestille dette fra lege eller annen behandler.
+              </BodyLong>
+            </li>
+            <li>
+              <Label>Vi vurderer mulighetene og begrensningene dine i møte med arbeidslivet</Label>
+              <BodyLong spacing>
+                Du har krav på en vurdering av hvilken oppfølging du trenger fra NAV for å komme i
+                eller beholde arbeid (folketrygdloven §14a). Vi bruker da legeerklæringen og
+                opplysningene du har gitt oss til å se på mulighetene dine til å jobbe. Hvis vi
+                trenger det, tar vi kontakt med deg for å få mer informasjon om situasjonen din. Du
+                får et brev med vurderingen vår. Hvis du ikke er enig i den,{' '}
+                <Link href="#">bør du klage på oppfølgingsvedtaket</Link>.
+              </BodyLong>
+            </li>
+            <li>
+              <Label>Hva kan du gjøre?</Label>
+              <BodyLong spacing>
+                Det er til hjelp for oss om du registrerer{' '}
+                <Link href="#">CV-en din på arbeidsplassen.no</Link>. Husk å laste opp vedlegg. Gi
+                beskjed hvis noe endrer seg.
+              </BodyLong>
+            </li>
+            <li>
+              <Label>Vi sjekker om du har rett på AAP</Label>
+              <BodyLong spacing>
+                Til sist sjekker vi om du har rett på AAP etter folketrygdloven kapittel 11. Vi
+                bruker her opplysningene vi har fått i forbindelse med søknaden.
+              </BodyLong>
+            </li>
+            <li>
+              <Label>Svar på AAP-søknaden</Label>
+              <BodyLong spacing>
+                Du får et vedtak med vurderingen vår av om du har rett på AAP eller ikke. Her står
+                det også hvor mye du vil få utbetalt. Hvis du ikke er enig,{' '}
+                <Link href="#">bør du klage på AAP-vedtaket</Link>.
+              </BodyLong>
+              <BodyLong spacing>
+                Du har rett til oppfølgning fra NAV uansett om du får innvilget AAP eller ikke.
+              </BodyLong>
+            </li>
+          </ul>
+        </Panel>
+
+        <Panel border>
+          <Heading level="2" size="medium" spacing>
+            {formatMessage('dokumentoversikt.tittel')}
+          </Heading>
           <BodyShort spacing>
             <Link href="#">{formatMessage('dokumentoversikt.ikkeSynligDokumentLink')}</Link>
           </BodyShort>
@@ -127,7 +186,7 @@ const Index = ({ søknader, dokumenter, mellomlagredeSøknader }: PageProps) => 
               </LinkPanel>
             ))}
           </VerticalFlexContainer>
-        </PanelWithTopIcon>
+        </Panel>
       </Section>
     </Layout>
   );
