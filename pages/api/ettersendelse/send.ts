9b969c91243afd2390abe8bd144ccdd268340460
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2022-09-01 08:37:23 +0200

    :hammer: Proxyer kall mot backend for å sende ettersendelse

--- pages/api/ettersendelse/send.ts
+++ pages/api/ettersendelse/send.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getAccessTokenFromRequest } from '../../../auth/accessToken';
 import { beskyttetApi } from '../../../auth/beskyttetApi';
+import { tokenXProxy } from '../../../auth/tokenXProxy';
 import { isMock } from '../../../utils/environments';
 
 const handler = beskyttetApi(async (req: NextApiRequest, res: NextApiResponse) => {
@@ -8,11 +9,18 @@ const handler = beskyttetApi(async (req: NextApiRequest, res: NextApiResponse) =
   res.status(201).json(await sendEttersendelse(req.body, accessToken));
 });
 
-const sendEttersendelse = async (data: string, accessToken?: string) => {
+export const sendEttersendelse = async (data: string, accessToken?: string) => {
   if (isMock()) {
     return {};
   }
-  return {};
+  const ettersendelse = await tokenXProxy({
+    url: `${process.env.SOKNAD_API_URL}/innsending/ettesend`,
+    method: 'POST',
+    data: JSON.stringify(data),
+    audience: process.env.SOKNAD_API_AUDIENCE!,
+    bearerToken: accessToken,
+  });
+  return ettersendelse;
 };
 
 export default handler;
