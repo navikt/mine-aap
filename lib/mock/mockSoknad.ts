949ff168c7da143d8913286fb3062bfcf8a7664c
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2022-09-13 14:00:44 +0200

    :clown_face: Oppdaterer mock-søknader til å bruke samme søknadId som fra mock i søknad

--- lib/mock/mockSoknad.ts
+++ lib/mock/mockSoknad.ts
@@ -3,12 +3,12 @@ import { Søknad } from 'lib/types/types';
 export const mockSøknader: Søknad[] = [
   {
     innsendtDato: '2022-08-30T10:54:49.737467',
-    søknadId: '32e3602e-7a7b-4440-89ca-8e2332d57bb9',
+    søknadId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
     innsendteVedlegg: [
-      { vedleggType: 'STUDIER', innsendtDato: '2022-08-30T10:54:51.034007' },
+      { vedleggType: 'UTLAND', innsendtDato: '2022-08-30T10:54:51.034007' },
       { vedleggType: 'ANNET', innsendtDato: '2022-08-30T10:54:51.047477' },
     ],
-    manglendeVedlegg: ['OMSORG'],
+    manglendeVedlegg: ['ARBEIDSGIVER'],
   },
   { innsendtDato: '2022-08-30T08:53:47.215149', søknadId: '835a12fc-e642-42da-b182-5169c488842f' },
 ];
