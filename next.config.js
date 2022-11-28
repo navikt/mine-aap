970e0bf1fb3619444793326ba02a12c589510eb7
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2022-11-28 10:44:17 +0100

    :children_crossing: Legger tilbake cdn nå som bygg skal fungere greit

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ const nextConfig = {
   trailingSlash: true,
   reactStrictMode: true,
   output: 'standalone',
+  assetPrefix: process.env.ASSET_PREFIX ?? undefined,
 
   i18n: {
     locales: ['nb', 'nn'],
