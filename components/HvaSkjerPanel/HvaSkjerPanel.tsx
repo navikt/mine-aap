a0efd4367b26c227b3e71146fa7d43c0b7bb6aee
vetleh <vetle.hollund@nav.no>
2023-02-03 13:59:30 +0100

    :recycle: fjernet toggleIntl på HvaskjerPanel

--- components/HvaSkjerPanel/HvaSkjerPanel.tsx
+++ components/HvaSkjerPanel/HvaSkjerPanel.tsx
@@ -1,82 +1,114 @@
 import { Panel, Heading, Label, BodyLong, BodyShort, Link } from '@navikt/ds-react';
 import styles from './HvaSkjerPanel.module.css';
 import { useFeatureToggleIntl } from 'lib/hooks/useFeatureToggleIntl';
+import { useIntl, FormattedMessage } from 'react-intl';
+import React from 'react';
 
 export const HvaSkjerPanel = () => {
-  const { formatMessage, formatElement } = useFeatureToggleIntl();
+  const intl = useIntl();
   return (
     <Panel className={styles.panel}>
       <Heading level="2" size="medium" spacing>
-        {formatMessage('hvaSkjerPanel.heading')}
+        <FormattedMessage id="hvaSkjerPanel.heading" />
       </Heading>
       <ul>
         <li>
-          <Label as={'p'}>{formatMessage('hvaSkjerPanel.punkt1.label')}</Label>
-          <BodyLong spacing>{formatMessage('hvaSkjerPanel.punkt1.tekst')}</BodyLong>
-        </li>
-        <li>
-          <Label as={'p'}>{formatMessage('hvaSkjerPanel.punkt2.label')}</Label>
+          <Label as={'p'}>
+            <FormattedMessage id="hvaSkjerPanel.punkt1.label" />
+          </Label>
           <BodyLong spacing>
-            {formatElement('hvaSkjerPanel.punkt2.tekst', {
-              // @ts-ignore-line
-              a: (chunks: string[]) => (
-                <Link target="_blank" href="https://klage.nav.no/nb/arbeid/nav-loven-14a">
-                  {chunks?.[0]}
-                </Link>
-              ),
-            })}
+            <FormattedMessage id="hvaSkjerPanel.punkt1.tekst" />
           </BodyLong>
         </li>
         <li>
-          <Label as={'p'}>{formatMessage('hvaSkjerPanel.punkt3.label')}</Label>
-          <BodyShort spacing>{formatMessage('hvaSkjerPanel.punkt3.tekst')}</BodyShort>
-          <ul>
-            <li>
-              {formatElement('hvaSkjerPanel.punkt3.punkt1', {
-                // @ts-ignore-line
-                a: (chunks: string[]) => (
-                  <Link target="_blank" href="https://arbeidsplassen.nav.no/">
-                    {chunks?.[0]}
+          <Label as={'p'}>
+            <FormattedMessage id="hvaSkjerPanel.punkt2.label" />
+          </Label>
+          <BodyLong spacing>
+            <FormattedMessage
+              id="hvaSkjerPanel.punkt2.tekst"
+              values={{
+                a: (chunks) => (
+                  <Link target="_blank" href="https://klage.nav.no/nb/arbeid/nav-loven-14a">
+                    {chunks}
                   </Link>
                 ),
-              })}
+              }}
+            />
+          </BodyLong>
+        </li>
+        <li>
+          <Label as={'p'}>
+            <FormattedMessage id="hvaSkjerPanel.punkt3.label" />
+          </Label>
+          <BodyShort spacing>
+            <FormattedMessage id="hvaSkjerPanel.punkt3.tekst" />
+          </BodyShort>
+          <ul>
+            <li>
+              <FormattedMessage
+                id="hvaSkjerPanel.punkt3.punkt1"
+                values={{
+                  a: (chunks) => (
+                    <Link target="_blank" href="https://arbeidsplassen.nav.no/">
+                      {chunks}
+                    </Link>
+                  ),
+                }}
+              />
             </li>
-            <li>{formatMessage('hvaSkjerPanel.punkt3.punkt2')}</li>
-            <li>{formatMessage('hvaSkjerPanel.punkt3.punkt3')}</li>
+            <li>
+              <FormattedMessage id="hvaSkjerPanel.punkt3.punkt2" />
+            </li>
+            <li>
+              <FormattedMessage id="hvaSkjerPanel.punkt3.punkt3" />
+            </li>
           </ul>
         </li>
         <li>
-          <Label as={'p'}>{formatMessage('hvaSkjerPanel.punkt4.label')}</Label>
-          <BodyLong spacing>{formatMessage('hvaSkjerPanel.punkt4.tekst')}</BodyLong>
+          <Label as={'p'}>
+            <FormattedMessage id="hvaSkjerPanel.punkt4.label" />
+          </Label>
+          <BodyLong spacing>
+            <FormattedMessage id="hvaSkjerPanel.punkt4.tekst" />
+          </BodyLong>
         </li>
         <li>
-          <Label as={'p'}>{formatMessage('hvaSkjerPanel.punkt5.label')}</Label>
+          <Label as={'p'}>
+            <FormattedMessage id="hvaSkjerPanel.punkt5.label" />
+          </Label>
           <BodyLong spacing>
-            {formatElement('hvaSkjerPanel.punkt5.tekst', {
-              // @ts-ignore-line
-              a: (chunks: string[]) => (
-                <Link
-                  target="_blank"
-                  href={
-                    'https://www.nav.no/soknader/nb/person/arbeid/arbeidsavklaringspenger/NAV%2011-13.05/klage-eller-anke/brev'
-                  }
-                >
-                  {chunks?.[0]}
-                </Link>
-              ),
-            })}
+            <FormattedMessage
+              id="hvaSkjerPanel.punkt5.tekst"
+              values={{
+                a: (chunks) => (
+                  <Link
+                    target="_blank"
+                    href={
+                      'https://www.nav.no/soknader/nb/person/arbeid/arbeidsavklaringspenger/NAV%2011-13.05/klage-eller-anke/brev'
+                    }
+                  >
+                    {chunks}
+                  </Link>
+                ),
+              }}
+            />
           </BodyLong>
           <BodyLong spacing>
-            {formatElement('hvaSkjerPanel.punkt5.tekst1', {
-              // @ts-ignore-line
-              a: (chunks: string[]) => (
-                <Link target="_blank" href={'https://www.nav.no/skattetrekk'}>
-                  {chunks?.[0]}
-                </Link>
-              ),
-            })}
+            <FormattedMessage
+              id="hvaSkjerPanel.punkt5.tekst1"
+              values={{
+                a: (chunks) => (
+                  <Link target="_blank" href={'https://www.nav.no/skattetrekk'}>
+                    {chunks}
+                  </Link>
+                ),
+              }}
+            />
           </BodyLong>
-          <BodyLong spacing>{formatMessage('hvaSkjerPanel.punkt5.tekst2')}</BodyLong>
+          <BodyLong spacing>
+            <FormattedMessage id="hvaSkjerPanel.punkt5.tekst2" />
+          </BodyLong>
         </li>
       </ul>
     </Panel>
