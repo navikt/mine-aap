a42af4472a887c66116648b746df7197e54ebb13
VHollund <vetlehoo@gmail.com>
2023-05-08 14:55:20 +0200

    :recycle: wrapper på tokenXProxy for metrics for sletting

--- pages/api/vedlegg/slett.ts
+++ pages/api/vedlegg/slett.ts
@@ -1,38 +1,28 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getCommaSeparatedStringFromStringOrArray } from '@navikt/aap-felles-utils-client';
-import {
-  logger,
-  isMock,
-  tokenXApiProxy,
-  beskyttetApi,
-  getAccessTokenFromRequest,
-} from '@navikt/aap-felles-innbygger-utils';
-import metrics from 'lib/metrics';
+import { isMock, beskyttetApi } from '@navikt/aap-felles-innbygger-utils';
+import { tokenXProxy } from '../../../lib/api/tokenXProxy';
 
 const handler = beskyttetApi(async (req: NextApiRequest, res: NextApiResponse) => {
   const uuids = req.query.uuid ?? [];
   if (!uuids) {
     res.status(400).json({ error: 'uuid må være en string' });
   }
-  const accessToken = getAccessTokenFromRequest(req);
-  await slettVedlegg(uuids, accessToken);
-  res.status(204).json({});
+  if (isMock()) return res.status(204).json({});
+  const commaSeparatedUuids = getCommaSeparatedStringFromStringOrArray(uuids);
+  return await tokenXProxy(
+    req,
+    res,
+    `/vedlegg/slett'?uuids=${commaSeparatedUuids}`,
+    '/vedlegg/slett'
+  );
 });
 
-export const slettVedlegg = async (uuids: string | string[], accessToken?: string) => {
-  if (isMock()) return;
-  const commaSeparatedUuids = getCommaSeparatedStringFromStringOrArray(uuids);
-  await tokenXApiProxy({
-    url: `${process.env.SOKNAD_API_URL}/vedlegg/slett?uuids=${commaSeparatedUuids}`,
-    prometheusPath: '/vedlegg/slett',
-    method: 'DELETE',
-    noResponse: true,
-    audience: process.env.SOKNAD_API_AUDIENCE!,
-    bearerToken: accessToken,
-    logger: logger,
-    metricsStatusCodeCounter: metrics.backendApiStatusCodeCounter,
-    metricsTimer: metrics.backendApiDurationHistogram,
-  });
+export const config = {
+  api: {
+    bodyParser: false,
+    externalResolver: true,
+  },
 };
 
 export default handler;
