3826595f549a2ff85c194a09216c4798829510b9
Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
2023-05-09 12:27:07 +0200

    logger resultat fra proxyapirouterequest

--- lib/api/tokenXProxy.ts
+++ lib/api/tokenXProxy.ts
@@ -25,6 +25,7 @@ export const tokenXProxy = async (
     bearerToken: tokenxToken,
     https: false,
   });
+  logger.info(`result from tokenXProxy: ${JSON.stringify(result)}`);
   stopTimer();
 
   return result;
