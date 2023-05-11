6ea3732ec2635c07cc53cdb664efbf03e24a5da3
VHollund <vetlehoo@gmail.com>
2023-05-11 09:46:14 +0200

    :recycle: 'vedlegg/les' bruker proxyAPIRequest pack

--- pages/api/vedlegg/les.ts
+++ pages/api/vedlegg/les.ts
@@ -1,38 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getStringFromPossiblyArrayQuery } from '@navikt/aap-felles-utils-client';
-import {
-  logger,
-  isMock,
-  tokenXApiProxy,
-  beskyttetApi,
-  getAccessTokenFromRequest,
-} from '@navikt/aap-felles-innbygger-utils';
-import metrics from 'lib/metrics';
+import { beskyttetApi } from '@navikt/aap-felles-innbygger-utils';
+import { tokenXProxy } from '../../../lib/api/tokenXProxy';
 
 const handler = beskyttetApi(async (req: NextApiRequest, res: NextApiResponse) => {
   const uuid = getStringFromPossiblyArrayQuery(req.query.uuid);
   if (!uuid) {
     res.status(400).json({ error: 'uuid må være en string' });
   }
-  const accessToken = getAccessTokenFromRequest(req);
-  const result = await lesVedlegg(uuid as string, accessToken);
-  res.status(200).send(result.body);
+  return await tokenXProxy(req, res, `/vedlegg/les/${uuid}`, '/vedlegg/les');
 });
-
-export const lesVedlegg = async (uuid: string, accessToken?: string) => {
-  if (isMock()) return await fetch('http://localhost:3000/aap/mine-aap/Rød.png');
-  return await tokenXApiProxy({
-    url: `${process.env.SOKNAD_API_URL}/vedlegg/les/${uuid}`,
-    prometheusPath: '/vedlegg/les/{uuid}',
-    method: 'GET',
-    audience: process.env.SOKNAD_API_AUDIENCE!,
-    bearerToken: accessToken,
-    rawResonse: true,
-    logger: logger,
-    metricsStatusCodeCounter: metrics.backendApiStatusCodeCounter,
-    metricsTimer: metrics.backendApiDurationHistogram,
-  });
-};
 
 export const config = {
   api: {
