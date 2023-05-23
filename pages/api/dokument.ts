8f2f8cce6269c887d01e332b739fd7f5277f61d1
Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
2023-05-23 14:00:04 +0200

    Rydder bort gammel kode

--- pages/api/dokument.ts
+++ pages/api/dokument.ts
@@ -1,13 +1,6 @@
 import { getStringFromPossiblyArrayQuery } from '@navikt/aap-felles-utils-client';
 import { NextApiRequest, NextApiResponse } from 'next';
-import {
-  logger,
-  isMock,
-  tokenXApiProxy,
-  beskyttetApi,
-  getAccessTokenFromRequest,
-} from '@navikt/aap-felles-innbygger-utils';
-import metrics from 'lib/metrics';
+import { beskyttetApi, getAccessTokenFromRequest } from '@navikt/aap-felles-innbygger-utils';
 import { tokenXProxy } from 'lib/api/tokenXProxy';
 
 const handler = beskyttetApi(async (req: NextApiRequest, res: NextApiResponse) => {
@@ -24,36 +17,7 @@ const handler = beskyttetApi(async (req: NextApiRequest, res: NextApiResponse) =
     `/oppslag/dokument/${journalpostId}/${dokumentId}`,
     '/oppslag/dokument'
   );
-  /*const result: Response = await lesDokument(
-    journalpostId as string,
-    dokumentId as string,
-    accessToken
-  );
-
-  res.setHeader('Content-Type', result.headers.get('Content-Type') ?? '');
-  res.setHeader('Content-Disposition', result.headers.get('Content-Disposition') ?? '');
-  res.status(200).send(result.body);
-  */
 });
-
-export const lesDokument = async (
-  journalpostId: string,
-  dokumentId: string,
-  accessToken?: string
-) => {
-  if (isMock()) return await fetch('http://localhost:3000/aap/mine-aap/Rød.png');
-  return await tokenXApiProxy({
-    url: `${process.env.SOKNAD_API_URL}/oppslag/dokument/${journalpostId}/${dokumentId}`,
-    prometheusPath: '/oppslag/dokument/{journalpostId}/{dokumentId}',
-    method: 'GET',
-    audience: process.env.SOKNAD_API_AUDIENCE!,
-    bearerToken: accessToken,
-    rawResonse: true,
-    logger: logger,
-    metricsStatusCodeCounter: metrics.backendApiStatusCodeCounter,
-    metricsTimer: metrics.backendApiDurationHistogram,
-  });
-};
 
 export const config = {
   api: {
