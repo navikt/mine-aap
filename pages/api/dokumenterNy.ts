0ca7b14da322a7ec08206852c3c3284878682991
Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
2023-06-09 11:16:56 +0200

    :alien: Mapper safResponse til Dokument[]

--- pages/api/dokumenterNy.ts
+++ pages/api/dokumenterNy.ts
@@ -1,24 +1,70 @@
-import { mockDokumenter } from 'lib/mock/mockDokumenter';
 import {
   logger,
-  isMock,
   tokenXApiProxy,
   beskyttetApi,
   getAccessTokenFromRequest,
 } from '@navikt/aap-felles-utils';
 import metrics from 'lib/metrics';
+import { Dokument } from 'lib/types/types';
+
+export interface SafResponse {
+  data: {
+    dokumentoversiktSelvbetjening: {
+      journalposter: [
+        {
+          journalpostId: string;
+          journalposttype: 'I' | 'U' | 'N';
+          journalstatus: string;
+          relevanteDatoer: [
+            {
+              dato: string;
+              datoType: string;
+            }
+          ];
+          tittel: string;
+          dokumenter: [
+            {
+              tittel: string;
+              dokumentInfoId: string;
+            }
+          ];
+        }
+      ];
+    };
+  };
+}
+
+export const mapSafResponseToDokumenter = (safResponse: SafResponse): Dokument[] => {
+  const dokumenter: Dokument[] = [];
+  safResponse.data.dokumentoversiktSelvbetjening.journalposter.forEach((journalpost) => {
+    journalpost.dokumenter.forEach((dokument) => {
+      dokumenter.push({
+        tittel: dokument.tittel,
+        dokumentId: dokument.dokumentInfoId,
+        journalpostId: journalpost.journalpostId,
+        type: journalpost.journalposttype,
+        dato:
+          journalpost.relevanteDatoer.find((dato) => dato.datoType === 'DATO_REGISTRERT')?.dato ??
+          '',
+        innsendingsId: '',
+      });
+    });
+  });
+  return dokumenter;
+};
 
 const handler = beskyttetApi(async (req, res) => {
   if (process.env.NEXT_PUBLIC_ENVIRONMENT != 'dev') {
     return res.status(404).json({ message: 'Not found' });
   }
   const accessToken = getAccessTokenFromRequest(req);
-  const dokumenter = await getDocuments(accessToken);
+  const safResponse = await getDocuments(accessToken);
+  const dokumenter = mapSafResponseToDokumenter(safResponse);
   res.status(200).json(dokumenter);
 });
 
-export const getDocuments = async (accessToken?: string) => {
-  if (isMock()) return mockDokumenter;
+export const getDocuments = async (accessToken?: string): Promise<SafResponse> => {
+  //if (isMock()) return mockDokumenter;
   return await tokenXApiProxy({
     url: `${process.env.DOKUMENTER_URL}/api/dokumenter`,
     prometheusPath: '/api/dokumenter',
