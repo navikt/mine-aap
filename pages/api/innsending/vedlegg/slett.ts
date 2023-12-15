c313549e207c01f72baedf3c37035a2503e98dd7
Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
2023-12-15 07:38:15 +0100

    Slett endepunkt skal fungere for sletting av vedlegg

--- pages/api/innsending/vedlegg/slett.ts
+++ pages/api/innsending/vedlegg/slett.ts
@@ -1,21 +1,36 @@
 import { getStringFromPossiblyArrayQuery } from '@navikt/aap-felles-utils-client';
-import { beskyttetApi, getAccessTokenFromRequest, tokenXApiProxy } from '@navikt/aap-felles-utils';
+import { beskyttetApi, getAccessTokenFromRequest, getTokenX } from '@navikt/aap-felles-utils';
+import { proxyApiRouteRequest } from '@navikt/next-api-proxy';
 
 const handler = beskyttetApi(async (req, res) => {
   const uuid = getStringFromPossiblyArrayQuery(req.query.uuid);
   if (!uuid) {
     res.status(400).json({ error: 'uuid må være en string' });
   }
-  const accessToken = getAccessTokenFromRequest(req);
-  return await tokenXApiProxy({
-    url: `${process.env.INNSENDING_URL}/mellomlagring/fil/${uuid}`,
-    method: 'DELETE',
-    prometheusPath: '/mellomlagring/fil',
-    req,
-    audience: process.env.INNSENDING_AUDIENCE!,
-    bearerToken: accessToken,
-    noResponse: true,
+
+  const accessToken = getAccessTokenFromRequest(req)?.substring('Bearer '.length)!;
+  let tokenxToken;
+  try {
+    tokenxToken = await getTokenX(accessToken, process.env.INNSENDING_AUDIENCE!);
+  } catch (err: any) {
+    console.log('getTokenXError', err);
+  }
+  return await proxyApiRouteRequest({
+    hostname: 'innsending',
+    path: `/mellomlagring/fil/${uuid}`,
+    req: req,
+    /* @ts-ignore: TODO */
+    res: res,
+    bearerToken: tokenxToken,
+    https: false,
   });
 });
+
+export const config = {
+  api: {
+    bodyParser: false,
+    externalResolver: true,
+  },
+};
 
 export default handler;
