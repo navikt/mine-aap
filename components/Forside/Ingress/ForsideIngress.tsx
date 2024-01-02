a60abd9a3eed3b0a44824a8d86fc0d2417e7775a
Øivind Stensrud <oivind.stensrud@nav.no>
2024-01-02 12:46:21 +0100

    :recycle: Bytter ut deprekert komponent
    Ingress er deprekert, bytter til BodyLong

--- components/Forside/Ingress/ForsideIngress.tsx
+++ components/Forside/Ingress/ForsideIngress.tsx
@@ -1,6 +1,8 @@
 import styles from './ForsideIngress.module.css';
-import { Ingress } from '@navikt/ds-react';
+import { BodyLong } from '@navikt/ds-react';
 
 export const ForsideIngress = ({ children }: { children: React.ReactNode }) => (
-  <Ingress className={styles.ingress}>{children}</Ingress>
+  <BodyLong size={'large'} className={styles.ingress}>
+    {children}
+  </BodyLong>
 );
