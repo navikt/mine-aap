626c4bb5181c283eb6755037dfc3cbb0e2318b49
Øivind Stensrud <oivind.stensrud@nav.no>
2024-01-04 16:09:58 +0100

    :white_check_mark: Forbedrer testdekning i applikasjonen
    Laget test for Soknad-komponenten

---
+++ components/Soknad/Soknad.test.tsx
@@ -0,0 +1,68 @@
+import { Soknad } from 'components/Soknad/Soknad';
+import {
+  søknadMedInnsendteOgManglendeVedlegg,
+  søknadMedInnsendteVedlegg,
+  søknadUtenVedlegg,
+} from 'lib/mock/mockSoknad';
+import { render, screen } from 'setUpTest';
+
+describe('Soknad', () => {
+  describe('alle søknader', () => {
+    test('har overskrift på nivå 2', () => {
+      render(<Soknad søknad={søknadMedInnsendteOgManglendeVedlegg} />);
+      expect(screen.getByRole('heading', { level: 2, name: 'Søknad om arbeidsavklaringspenger (AAP)' })).toBeVisible();
+    });
+
+    test('har knapp for å ettersende dokumentasjon', () => {
+      render(<Soknad søknad={søknadMedInnsendteOgManglendeVedlegg} />);
+      expect(screen.getByRole('button', { name: 'Ettersend dokumentasjon' })).toBeVisible();
+    });
+
+    test('viser en liste med mottatte dokumenter', () => {
+      render(<Soknad søknad={søknadMedInnsendteOgManglendeVedlegg} />);
+      expect(screen.getByText('Dette har vi mottatt fra deg')).toBeVisible();
+      expect(screen.getByRole('link', { name: 'Søknad om AAP' })).toBeVisible();
+      expect(screen.getByRole('link', { name: 'Dokumentasjon fra arbeidsgiver' })).toBeVisible();
+      expect(screen.getByRole('link', { name: 'Annen dokumentasjon' })).toBeVisible();
+    });
+
+    test('viser ingen liste dersom det ikke er sendt vedlegg', () => {
+      render(<Soknad søknad={søknadUtenVedlegg} />);
+      expect(screen.queryByText('Dette har vi mottatt fra deg')).not.toBeInTheDocument();
+    });
+  });
+
+  describe('for søknad med manglende vedlegg', () => {
+    test('vises varsel om manglende dokumentasjon', () => {
+      render(<Soknad søknad={søknadMedInnsendteOgManglendeVedlegg} />);
+      expect(
+        screen.getByText(
+          'Vi mangler dokumentasjon fra deg for å kunne behandle søknaden. Ettersend dette til oss så raskt du kan.'
+        )
+      ).toBeVisible();
+    });
+
+    test('vises en liste over dokumentasjon som mangler', () => {
+      render(<Soknad søknad={søknadMedInnsendteOgManglendeVedlegg} />);
+      expect(screen.getByText('Dokumentasjon vi mangler')).toBeVisible();
+      expect(
+        screen.getAllByRole('listitem').find((element) => element.textContent === 'Lønn og andre goder')
+      ).toBeVisible();
+    });
+  });
+
+  describe('uten manglende vedlegg', () => {
+    test('vises ingen liste over manglende dokumentasjon', () => {
+      render(<Soknad søknad={søknadMedInnsendteVedlegg} />);
+    });
+
+    test('vises ikke varsel om manglende dokumentasjon', () => {
+      render(<Soknad søknad={søknadMedInnsendteVedlegg} />);
+      expect(
+        screen.queryByText(
+          'Vi mangler dokumentasjon fra deg for å kunne behandle søknaden. Ettersend dette til oss så raskt du kan.'
+        )
+      ).not.toBeInTheDocument();
+    });
+  });
+});
--- lib/mock/mockSoknad.ts
+++ lib/mock/mockSoknad.ts
@@ -1,6 +1,5 @@
 import { Søknad } from 'lib/types/types';
-
-export const mockSøknader: Søknad[] = [
+const mockSøknader: Søknad[] = [
   {
     innsendtDato: '2022-08-30T10:54:49.737467',
     søknadId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
@@ -35,5 +34,38 @@ export const mockSøknader: Søknad[] = [
   { innsendtDato: '2022-08-30T08:53:47.215149', søknadId: '835a12fc-e642-42da-b182-5169c488842f' },
 ];
 
-export const søknadMedInnsendteOgManglendeVedlegg: Søknad = mockSøknader[0];
-export const søknadUtenVedlegg: Søknad = mockSøknader[1];
+const søknadMedInnsendteOgManglendeVedlegg: Søknad = mockSøknader[0];
+const søknadUtenVedlegg: Søknad = mockSøknader[1];
+
+const søknadMedInnsendteVedlegg: Søknad = {
+  innsendtDato: '2022-08-30T10:54:49.737467',
+  søknadId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+  innsendteVedlegg: [
+    {
+      journalpostId: '1',
+      dokumentId: '1',
+      tittel: 'Søknad om AAP',
+      type: 'I',
+      innsendingsId: '',
+      dato: '2022-08-30T08:53:47.000Z',
+    },
+    {
+      journalpostId: '111',
+      dokumentId: '222',
+      tittel: 'Dokumentasjon fra arbeidsgiver',
+      type: 'I',
+      innsendingsId: 'f3297977-618e-44e2-9ad4-9749b5790b35',
+      dato: '2022-09-30T10:54:51.034007',
+    },
+    {
+      journalpostId: '333',
+      dokumentId: '444',
+      tittel: 'Annen dokumentasjon',
+      type: 'I',
+      innsendingsId: 'ef4114c6-2308-445b-a5ff-1bac6b85873f',
+      dato: '2022-09-30T10:54:51.047477',
+    },
+  ],
+};
+
+export { mockSøknader, søknadMedInnsendteVedlegg, søknadUtenVedlegg, søknadMedInnsendteOgManglendeVedlegg };
