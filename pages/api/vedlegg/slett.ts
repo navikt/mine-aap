090914ce75bfea1b44a2eaf3cc0f80be2ea62f04
Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
2024-01-04 08:37:19 +0100

    :alien: Legger til feature-toggling for ny innsending og implementerer nytt endepunkt for å slette

--- pages/api/vedlegg/slett.ts
+++ pages/api/vedlegg/slett.ts
@@ -5,8 +5,11 @@ import {
   tokenXApiProxy,
   beskyttetApi,
   getAccessTokenFromRequest,
+  getTokenX,
 } from '@navikt/aap-felles-utils';
 import metrics from 'lib/metrics';
+import { proxyApiRouteRequest } from '@navikt/next-api-proxy';
+import { NextApiRequest, NextApiResponse } from 'next';
 
 const handler = beskyttetApi(async (req, res) => {
   const uuids = req.query.uuid ?? [];
@@ -14,9 +17,39 @@ const handler = beskyttetApi(async (req, res) => {
     res.status(400).json({ error: 'uuid må være en string' });
   }
   const accessToken = getAccessTokenFromRequest(req);
+  if (process.env.NY_INNSENDING === 'enabled') {
+    // @ts-ignore-line TODO: Feil med type for NextApiResponse vi henter fra felleslib
+    return await slettVedleggInnsending(uuids as string, accessToken!, req, res);
+  }
   await slettVedlegg(uuids, accessToken);
   res.status(204).json({});
 });
+
+export const slettVedleggInnsending = async (
+  uuid: string,
+  accessToken: string,
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  if (isMock()) return;
+
+  let tokenXToken;
+  try {
+    tokenXToken = await getTokenX(accessToken, process.env.INNSENDING_AUDIENCE!);
+  } catch (error) {
+    logger.error('Kunne ikke hente tokenXToken', error);
+    throw error;
+  }
+
+  return await proxyApiRouteRequest({
+    hostname: 'innsending',
+    path: `/mellomlarging/fil/${uuid}`,
+    req: req,
+    res: res,
+    bearerToken: tokenXToken,
+    https: false,
+  });
+};
 
 export const slettVedlegg = async (uuids: string | string[], accessToken?: string) => {
   if (isMock()) return;
@@ -32,6 +65,13 @@ export const slettVedlegg = async (uuids: string | string[], accessToken?: strin
     metricsStatusCodeCounter: metrics.backendApiStatusCodeCounter,
     metricsTimer: metrics.backendApiDurationHistogram,
   });
+};
+
+export const config = {
+  api: {
+    bodyParser: false,
+    externalResolver: true,
+  },
 };
 
 export default handler;
