5a862fbcbe9d66471bd93d8628f12cdd7f54fabf
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-01-16 16:12:37 +0100

    :alien: Legger til Nav-CallId i request

--- pages/api/dokument.ts
+++ pages/api/dokument.ts
@@ -1,6 +1,7 @@
 import { getStringFromPossiblyArrayQuery } from '@navikt/aap-felles-utils-client';
 import { beskyttetApi } from '@navikt/aap-felles-utils';
 import { tokenXProxy } from 'lib/api/tokenXProxy';
+import { randomUUID } from 'crypto';
 
 const handler = beskyttetApi(async (req, res) => {
   const journalpostId = getStringFromPossiblyArrayQuery(req.query.journalpostId);
@@ -8,6 +9,9 @@ const handler = beskyttetApi(async (req, res) => {
   if (!journalpostId || !dokumentId) {
     res.status(400).json({ error: 'journalpostId og dokumentId må være satt' });
   }
+
+  const callid = randomUUID();
+  req.headers['Nav-CallId'] = callid;
 
   return await tokenXProxy(
     req,
