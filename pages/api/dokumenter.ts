bff641deebad2d521b7c8b3b8adaa4b5e4e32d22
Øivind Stensrud <oivind.stensrud@nav.no>
2024-01-29 13:54:27 +0100

    :alien: Tar i bruk nye endepunkter for oppslag på dokumenter
    Co-authored-by: Thomas Rognes <thomas.sebastian.rognes@nav.no>
    Co-authored-by: Tor Idland <tor.idland@nav.no>

--- pages/api/dokumenter.ts
+++ pages/api/dokumenter.ts
@@ -1,38 +1,24 @@
 import { mockDokumenter } from 'lib/mock/mockDokumenter';
 import { logger, isMock, tokenXApiProxy, beskyttetApi, getAccessTokenFromRequest } from '@navikt/aap-felles-utils';
 import metrics from 'lib/metrics';
-import { hentDokumenterFraOppslag } from 'pages/api/nyeapi/dokumenter';
 
 const handler = beskyttetApi(async (req, res) => {
   const accessToken = getAccessTokenFromRequest(req);
-  const dokumenter = await getDocuments(accessToken);
-
-  try {
-    const dokumenterFraOppslag = await hentDokumenterFraOppslag(accessToken);
-    if (dokumenter.length !== dokumenterFraOppslag.length) {
-      logger.warn(
-        `Dokumenter fra oppslag og dokumenter fra soknad-api er ikke lik. dokumenter fra soknad-api har antall ${dokumenter.length}, og dokumenter fra oppslag har antall ${dokumenterFraOppslag.length}.`
-      );
-    }
-  } catch (e) {
-    logger.error('Noe gikk galt i kallet mot oppslag for dokumenter:', e);
-  }
-
+  const dokumenter = await hentDokumenterFraOppslag(accessToken);
   res.status(200).json(dokumenter);
 });
 
-export const getDocuments = async (accessToken?: string) => {
+export const hentDokumenterFraOppslag = async (accessToken?: string) => {
   if (isMock()) return mockDokumenter;
   return await tokenXApiProxy({
-    url: `${process.env.SOKNAD_API_URL}/oppslag/dokumenter`,
+    url: `${process.env.OPPSLAG_URL}/dokumenter`,
     prometheusPath: '/oppslag/dokumenter',
     method: 'GET',
-    audience: process.env.SOKNAD_API_AUDIENCE!,
+    audience: process.env.OPPSLAG_AUDIENCE!,
     bearerToken: accessToken,
     logger: logger,
     metricsStatusCodeCounter: metrics.backendApiStatusCodeCounter,
     metricsTimer: metrics.backendApiDurationHistogram,
   });
 };
-
 export default handler;
