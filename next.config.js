766dbdb08c9a4dc110325a6361760ecddd1d0db6
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-02-14 21:06:01 +0100

    Legger til nøyaktig path i Access-control-allow-origin

--- next.config.js
+++ next.config.js
@@ -23,7 +23,7 @@ const nextConfig = {
         source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Origin', value: 'https://www.intern.dev.nav.no' },
           { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
           {
             key: 'Access-Control-Allow-Headers',
