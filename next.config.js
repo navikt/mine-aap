da5d314f4757493b8a0f479bbb65247f24aec43b
Øivind Stensrud <oivind.stensrud@nav.no>
2024-02-14 15:52:43 +0100

    :passport_control: Justering av CORS-oppsett
    Co-authored-by: Thomas Rognes <thomas.sebastian.rognes@nav.no>
    Co-authored-by: Robin Tordly <robin.tordly@nav.no>

--- next.config.js
+++ next.config.js
@@ -18,7 +18,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/api/:path*',
+        source: '/aap/mine-aap/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
