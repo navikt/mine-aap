2fcddbc7e84b5819f2a441c84089bdbd1c03b0f3
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-02-14 20:43:29 +0100

    test

--- next.config.js
+++ next.config.js
@@ -15,14 +15,16 @@ const nextConfig = {
     locales: ['nb', 'nn'],
     defaultLocale: 'nb',
   },
+
   async headers() {
     return [
       {
-        source: '/aap/mine-aap/api/:path*',
+        // matching all API routes
+        source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
           {
             key: 'Access-Control-Allow-Headers',
             value:
