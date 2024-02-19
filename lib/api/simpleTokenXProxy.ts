072c38b4a94c75d94e17485ca625361edd3d35ce
Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
2024-02-19 12:32:22 +0100

    :alien: Enklere proxy som bruker oasis for token exchange mot tokenX
    Co-authored-by: Tor Idland <tor.idland@nav.no>
    Co-authored-by: Robin Tordly <robin.tordly@nav.no>
    Co-authored-by: Sturle Helland <sturle.helland@nav.no>

---
+++ lib/api/simpleTokenXProxy.ts
@@ -0,0 +1,37 @@
+import { logger } from '@navikt/aap-felles-utils';
+import { getSession } from '@navikt/oasis';
+import { randomUUID } from 'crypto';
+import { IncomingMessage } from 'http';
+
+interface Opts {
+  url: string;
+  audience: string;
+  req?: IncomingMessage;
+}
+
+export const simpleTokenXProxy = async ({ url, audience, req }: Opts) => {
+  if (!req) {
+    logger.error(`Request for ${url} er undefined`);
+    throw new Error('Request for simpleTokenXProxy is undefined');
+  }
+
+  const session = await getSession(req);
+  const onBehalfOfToken = await session.apiToken(audience);
+
+  const navCallId = randomUUID();
+
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${onBehalfOfToken}`,
+      'Content-Type': 'application/json',
+      'Nav-CallId': navCallId,
+    },
+  });
+
+  if (response.ok) {
+    return await response.json();
+  }
+  logger.error('Error fetching simpleTokenXProxy');
+  throw new Error('Error fetching simpleTokenXProxy');
+};
