34cb0577830412347d8a08e9f0b21a1602e95d26
Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
2024-06-26 10:24:44 +0200

    :lock: Legger til csp headere fra dekoratør slik at denne rendres serverside i dev (#361)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
+const ssr = require('@navikt/nav-dekoratoren-moduler/ssr');
+
+const appDirectives = {
+  'connect-src': ["'self'"],
+  'font-src': ['https://fonts.gstatic.com'],
+  'object-src': ['none'],
+  'script-src-elem': ["'self'"],
+  'style-src-elem': ["'self'"],
+  'img-src': ["'self'", 'data:', 'blob:'],
+};
 
 const nextConfig = {
   basePath: '/aap/mine-aap',
@@ -9,6 +19,21 @@ const nextConfig = {
 
   experimental: {
     instrumentationHook: true,
+  },
+
+  async headers() {
+    const csp = await ssr.buildCspHeader(appDirectives, { env: process.env.DECORATOR_ENV ?? 'prod' });
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: csp,
+          },
+        ],
+      },
+    ];
   },
 
   i18n: {
