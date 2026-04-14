2cc0e9568982333730bea4c70b54c7ab0cbb627f
Tor Idland <tor.idland@nav.no>
2026-04-14 19:35:34 +0200

    riktig type for request og query params i promise

--- app/api/dokument/[journalPostId]/[dokumentInfoId]/route.ts
+++ app/api/dokument/[journalPostId]/[dokumentInfoId]/route.ts
@@ -1,10 +1,8 @@
 import { hentDokument } from 'lib/services/oppslagService';
-import { NextApiRequest } from 'next';
+import {NextRequest} from "next/server";
 
-export async function GET(
-  req: NextApiRequest,
-  { params }: { params: { journalPostId: string; dokumentInfoId: string } }
-) {
+export async function GET(_: NextRequest, props: { params: Promise<{ journalPostId: string; dokumentInfoId: string }> }) {
+  const params = await props.params;
   const res = await hentDokument(params.journalPostId, params.dokumentInfoId);
   const blob = await res.blob();
 
