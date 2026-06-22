36ffb94c46b5dd41b41eefa66d75b85f9af30cdc
Tor Idland <tor.idland@nav.no>
2026-06-22 15:31:05 +0200

    fjerner res.blob() kall i api route, slik at vi unngår å lese fil inn i minne

--- app/api/dokument/[journalPostId]/[dokumentInfoId]/route.ts
+++ app/api/dokument/[journalPostId]/[dokumentInfoId]/route.ts
@@ -6,14 +6,5 @@ export async function GET(
   props: { params: Promise<{ journalPostId: string; dokumentInfoId: string }> }
 ) {
   const params = await props.params;
-  const res = await hentDokument(params.journalPostId, params.dokumentInfoId);
-  const blob = await res.blob();
-
-  if (blob !== undefined) {
-    return new Response(blob, { status: 200, headers: res.headers });
-  } else {
-    return new Response(JSON.stringify({ message: 'Ingen dokument funnet.' }), {
-      status: 500,
-    });
-  }
+  return hentDokument(params.journalPostId, params.dokumentInfoId);
 }
