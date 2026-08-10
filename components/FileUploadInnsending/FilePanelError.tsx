41177524038cdb58055c36d15e0e47338e73ff24
Øivind Stensrud <93574268+steoiv@users.noreply.github.com>
2026-08-10 07:16:02 +0200

    AAP-2194 Fjerner aap-felles-css (#833)
    Flytter det lille av css som kom derfra inn hit.

--- components/FileUploadInnsending/FilePanelError.tsx
+++ components/FileUploadInnsending/FilePanelError.tsx
@@ -2,6 +2,8 @@ import { ArrowUndoIcon, FileTextIcon } from '@navikt/aksel-icons';
 import { BodyShort, Label, Panel } from '@navikt/ds-react';
 import type { Vedlegg } from './FileInputInnsending';
 
+import styles from './FileUploadInnsending.module.css';
+
 interface Props {
   file: Vedlegg;
   onDelete: () => void;
@@ -10,16 +12,16 @@ interface Props {
 export const FilePanelError = ({ file, onDelete }: Props) => {
   return (
     <>
-      <Panel className={'fileCard error'} tabIndex={0}>
-        <div className={'fileCardLeftContent'}>
-          <div className={'fileError'}>
+      <Panel className={`${styles.fileCard} ${styles.error}`} tabIndex={0}>
+        <div className={styles.fileCardLeftContent}>
+          <div className={styles.fileError}>
             <FileTextIcon color={'var(--a-surface-danger-hover)'} />
           </div>
           <div>
             <Label as={'span'}>{file.name}</Label>
           </div>
         </div>
-        <button type={'button'} onClick={onDelete} tabIndex={0} className={'deleteAttachment'}>
+        <button type={'button'} onClick={onDelete} tabIndex={0} className={styles.deleteAttachment}>
           <ArrowUndoIcon title={'Avbryt'} />
           <BodyShort>{'Avbryt'}</BodyShort>
         </button>
